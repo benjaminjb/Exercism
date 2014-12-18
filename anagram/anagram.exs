ad61ddfb9af53bcd7f167030e2187d40ec563003
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-12-18 09:32:27 -0600

    Exercism Elixir challenge -- Anagram v1

--- anagram/anagram.exs
+++ anagram/anagram.exs
@@ -4,6 +4,27 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
-  
+    check_anagram(base, candidates, [])
   end
+
+  def check_anagram(base, [], results) do 
+    Enum.uniq results
+    |> Enum.reverse
+  end
+  
+  def check_anagram(base, [candidate_head | candidate_tail], results) do
+    if separated_and_sorted(base) == separated_and_sorted(candidate_head) do
+      check_anagram(base, candidate_tail, [candidate_head | results])
+    else
+      check_anagram(base, candidate_tail, results)
+    end
+  end
+
+  defp separated_and_sorted(word) do
+    Kernel.to_string(word)
+    |> String.downcase
+    |> String.graphemes
+    |> Enum.sort
+  end
+
 end
