d890809b42d0b8e01a634ff9b82c53c1007deb36
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-12-23 13:32:24 -0600

    Exercism Elixir: Gigasecond, v1

---
+++ gigasecond/gigasecond.exs
@@ -0,0 +1,15 @@
+defmodule Gigasecond do
+	@doc """
+	Calculate a date one billion seconds after an input date.
+	"""
+	@spec from({pos_integer, pos_integer, pos_integer}) :: :calendar.date
+
+	def from({year, month, day}) do
+    {date, _time} = 
+    :calendar.datetime_to_gregorian_seconds({{year,month,day},{0,0,0}}) + :math.pow(10,9)
+    |> Float.to_string([decimals: 0]) 
+    |> String.to_integer
+    |> :calendar.gregorian_seconds_to_datetime
+    date
+	end
+end
