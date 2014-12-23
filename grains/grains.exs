bfe893a05c36c222e05a40a20ac4e0ee72f07e32
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-12-23 12:48:53 -0600

    Exercism Elixir: grains (formatting issue)

---
+++ grains/grains.exs
@@ -0,0 +1,17 @@
+defmodule Grains do
+  @doc """
+  Calculate two to the power of the input minus one.
+  """
+  @spec square(pos_integer) :: pos_integer
+  def square(number) do
+    :math.pow(2, (number - 1))
+  end
+
+  @doc """
+  Adds square of each number from 1 to 64.
+  """
+  @spec total :: pos_integer
+  def total do
+    Enum.reduce(0..63, 0, fn(x, acc) -> acc + :math.pow(2, x) end)
+  end
+end
