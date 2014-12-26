c65a4ec5f9a663c8104ec7bc523bf9d8d3c50072
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-12-26 11:16:23 -0600

    Exercism Elixir: Prime factors, v1

---
+++ prime-factors/prime_factors.exs
@@ -0,0 +1,25 @@
+defmodule PrimeFactors do
+  @doc """
+  Compute the prime factors for 'number'.
+
+  The prime factors are prime numbers that when multiplied give the desired
+  number.
+
+  The prime factors of 'number' will be ordered lowest to highest. 
+  """
+  @spec factors_for(pos_integer) :: [pos_integer]
+  def factors_for(number) do
+    factor(number, 2, [])
+  end
+
+  defp factor(1, _, _), do: []
+  defp factor(number, number, factor_list), do: Enum.reverse([number | factor_list])
+  
+  defp factor(number, prime, factor_list) when rem(number, prime) == 0 do
+    factor(div(number, prime), prime, [prime | factor_list])
+  end
+
+  defp factor(number, prime, factor_list) do
+    factor(number, prime + 1, factor_list)
+  end
+end
