2864955715c3bcabe533e279f5454b6167fb90db
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-12-26 10:19:58 -0600

    Exercism Elixir: Roman numerals, with Enum.map_join

--- roman-numerals/roman.exs
+++ roman-numerals/roman.exs
@@ -7,49 +7,42 @@ defmodule Roman do
     number
     |> Integer.to_string
     |> String.graphemes
-    |> transform
-    |> Enum.join ""
-  end
-
-  defp transform(number_list) do
-    number_list
+    |> Enum.reverse
     |> Enum.with_index
-    |> Enum.map(fn({x, y}) -> 
-      {x, length(number_list) - y}
-      end)
-    |> Enum.map(&transform_tuple(&1))
+    |> Enum.map_join(&transform_tuple/1)
+    |> String.reverse
   end
 
-  defp transform_tuple({num, pos}) when pos >= 1 and pos <= 4 do
-    
+  defp transform_tuple({num, pos}) when pos >= 0 and pos <= 3 do
     case pos do
-      4 -> 
-        char = "M"
       3 -> 
+        char = "M"
+      2 -> 
         upper = "M"
         half = "D"
         char = "C"
-      2 -> 
+      1 -> 
         upper = "C"
         half = "L"
         char = "X"
-      1 ->
+      0 ->
         upper = "X"
         half = "V"
         char = "I"
     end
 
+    num = String.to_integer(num)
     cond do
       num in 1..3   ->
         String.duplicate(char, num)
       num == 4      ->
-        "#{char}#{half}"
+        "#{half}#{char}"
       num == 5      ->
         "#{half}"
       num in 6..8   ->
-        "#{half}#{String.duplicate(char, num - 5)}"
+        "#{String.duplicate(char, num - 5)}#{half}"
       num == 9      ->
-        "#{char}#{upper}"
+        "#{upper}#{char}"
       num == 0      ->
         ""
     end
